05f0166ac8add278c15515596ffdee8ceae63aac
Diogo Pinheiro Murta Freire <diogo.freire@capgemini.com>
2025-05-12 18:33:08 -0300

    Fix: update files

--- backend-terraform.tf
+++ backend-terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "meu-bucket-terraform-state"
+    bucket = "meu-bucket2-terraform-state"
     key    = "global/s3/terraform.tfstate"
     region = "us-east-1"
     encrypt = true
