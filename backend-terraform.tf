a950f35338054b8b801d26fa0218b87f14239d65
Diogo Pinheiro Murta Freire <diogo.freire@capgemini.com>
2025-05-12 18:46:23 -0300

    Fix: terraform format

--- backend-terraform.tf
+++ backend-terraform.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "meu-bucket2-terraform-state"
-    key    = "global/s3/terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
+    bucket       = "meu-bucket2-terraform-state"
+    key          = "global/s3/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
     use_lockfile = true
-    
+
   }
 }
