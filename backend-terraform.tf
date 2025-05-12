5a297db46d250987f77c7b641393aa3681a51f43
Diogo Pinheiro Murta Freire <diogo.freire@capgemini.com>
2025-05-12 15:29:19 -0300

    Add files .tf

---
+++ backend-terraform.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "meu-bucket-terraform-state"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    use_lockfile = true
+    
+  }
+}
